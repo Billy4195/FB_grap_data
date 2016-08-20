3de4316a0aab7909537e3cde1631756ee3101f33
Billy.Su <g4691821@gmail.com>
2016-08-20 09:42:20 +0800

    add get token action & show groups page

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,12 +1,22 @@
 class AuthController < ApplicationController
   def index
-    @oauth = Koala::Facebook::OAuth.new('1736802146607522','b17e461faf7cd2b00d7f517ef93a2320','http://localhost:3000/')
+    @oauth = Koala::Facebook::OAuth.new('1736802146607522','b17e461faf7cd2b00d7f517ef93a2320','http://localhost:3000/auth/get_token')
     @URL = @oauth.url_for_oauth_code(:permission => "user_managed_groups")
-    
-    if( params[:code] )
-      @token = @oauth.get_access_token(params[:code])
-      session[:token] = @token
+  end
+  def get_token
+
+    @oauth = Koala::Facebook::OAuth.new('1736802146607522','b17e461faf7cd2b00d7f517ef93a2320','http://localhost:3000/auth/get_token')
+    @token = @oauth.get_access_token(params[:code])
+    session[:token] = @token
+
+    redirect_to :action => 'show_groups'
+  end
+  def show_groups
+    if( session[:token] )
+      @gapi = Koala::Facebook::API.new(session[:token])
+      @groups = @gapi.get_connection('me','groups',api_version: "2.6")
     end
+
   end
 
 end
